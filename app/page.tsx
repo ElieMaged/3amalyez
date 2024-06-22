2c5856771061793e81b8ac0bbf4fb8349c211da0
ElieMaged <Narutossj23@yahoo.com>
2024-06-22 19:06:16 +0300

    try again

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const handleClick = (e:any) => {
   <main className=" ">
  {/* Search:<input onChange={e => setQuery(e.target.value)} className='text-black'/> */}
  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose an option</label>
-  <select value ={query} id='mySelect' onChange={e=> setQuery(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  <select value ={query} id='mySelect' onChange={(e)=> setQuery(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option value='' >Choose a country</option>
     <option value="Egypt">Egypt</option>
     <option value="UAE">UAE</option>
