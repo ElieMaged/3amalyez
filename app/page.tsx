c5c0c6bbcbe1d3803b2614e4b4daa4776b5c2584
ElieMaged <Narutossj23@yahoo.com>
2024-06-22 19:37:34 +0300

    er

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ const [query, setQuery] = useState('')
 
 
 const filterItems = list.filter(prev => {
-  const pepe =  prev.toLowerCase();
-   pepe.includes(query.toLowerCase())})
+
+  prev.toLowerCase().includes(query.toLowerCase())})
 
 const valRef:any = useRef()
 // button Function
@@ -28,7 +28,7 @@ const handleClick = (e:any) => {
   
   return (
   <main className=" ">
- {/* Search:<input onChange={e => setQuery(e.target.value)} className='text-black'/> */}
+
  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose an option</label>
   <select value ={query} id='mySelect' onChange={(e)=> setQuery(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option value='' >Choose a country</option>
