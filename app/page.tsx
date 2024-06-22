707fa061767a44095ba63a907d8ce7460de459c6
ElieMaged <Narutossj23@yahoo.com>
2024-06-22 14:52:30 +0300

    q

--- app/page.tsx
+++ app/page.tsx
@@ -29,15 +29,15 @@ const handleClick = (e) => {
   return (
   <main className=" ">
  {/* Search:<input onChange={e => setQuery(e.target.value)} className='text-black'/> */}
- <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-  <select value ={query} id='mySelect' onChange={e => setQuery(e.target.value)} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+ <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+  <select value ={query} id='mySelect' onChange={e => setQuery(e.target.value)} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option value='' >Choose a country</option>
     <option value="Egypt">Egypt</option>
     <option value="UAE">UAE</option>
     <option value="Saudi Arabia">Saudi Arabia</option>
    </select>
 
-<form class="max-w-sm mx-auto">
+<form className="max-w-sm mx-auto">
 
 </form>
 
