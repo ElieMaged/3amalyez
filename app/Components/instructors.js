0d2b29fe092d0ad32826c444ef22f1eee71e85b9
ElieMaged <Narutossj23@yahoo.com>
2024-06-23 19:52:20 +0300

    yay!

--- app/Components/instructors.js
+++ app/Components/instructors.js
@@ -6,7 +6,8 @@ const instructors =  [
         courses: "Business",
         price: "0",
         img:"https://pbs.twimg.com/media/F_3cCa9WcAAGKkF.jpg",
-        id:1
+        id:1,
+        courseTag:'BUS'
     },
     {
         name: "Harambe",
@@ -15,7 +16,8 @@ const instructors =  [
         courses: "Medicine",
         price: "500",
         img:"https://ichef.bbci.co.uk/news/976/cpsprodpb/D2D2/production/_118707935_untitled-1.jpg",
-        id:2
+        id:2,
+        courseTag:'MED'
     },
     {
         name: "Haru",
@@ -24,7 +26,8 @@ const instructors =  [
         courses: "Woof Woof?",
         price: "100",
         img:"https://media.istockphoto.com/id/168362733/photo/english-cocker-spaniel.jpg?s=612x612&w=0&k=20&c=FeDtTQrcejqCc1bpuvhI3flpsonqP9M-P1GkTXdTE50=",
-        id:3
+        id:3,
+        courseTag:'WOOF'
     },
     {
         name: "Shrek",
@@ -33,7 +36,8 @@ const instructors =  [
         courses: "Rizzardry",
         price: "50",
         img:"https://i.redd.it/t8pxmqcmysf61.jpg",
-        id:4
+        id:4,
+        courseTag:'RIZZ'
     },
     {
         name: "Arty",
@@ -42,7 +46,8 @@ const instructors =  [
         courses: "Satanism",
         price: "200",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKK1zSX_YjOvNSHlqwuy84X_WMWRSHNigzpA&s",
-        id:5
+        id:5,
+        courseTag:'SATAN'
     }
 
 ]
--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,10 @@ export default function Home() {
 const [list, setList] = useState(mappy);
 const [query, setQuery] = useState('')
 
+const filterCourses = list.filter((prev) => {
+  return prev.courseTag.toLowerCase().includes(query.toLowerCase())
+})
+
 
 
 
@@ -26,11 +30,24 @@ const valRef:any = useRef()
 
   return (
   <main className=" ">
+    <br/>
+{/* Search for :<input value={query} onChange={e => setQuery(e.target.value)} className='text-black' text='search'></input> */}
+<br></br>
+<br></br>
+<br></br>
+<div className='flex flex-col'>
+ <h1 className='ml-2 text-2xl'>Find your Instructor:</h1>
+<select value={query} onChange={e => setQuery(e.target.value)} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2 w-1/3 ' name="cars" id="cars">
+<option selected className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white' value="">All Subjects</option>
+  <option className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white' value="BUS">Business</option>
+  <option className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white' value="MED">Medicine</option>
+  <option className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white' value="WOOF">Woof Woof?</option>
+  <option className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white' value="RIZZ">Rizzardry</option>
+  <option className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white' value="SATAN">Satanism</option>
+</select>
 
 
-
-
-{list.map(({name, occupation, country, courses, price, img, id}) => {
+{filterCourses.map(({name, occupation, country, courses, price, img, id}) => {
   return <Card
   key={id}
   name={name}
@@ -43,6 +60,7 @@ const valRef:any = useRef()
   />
 }
 )}
+</div>
 
 
 
