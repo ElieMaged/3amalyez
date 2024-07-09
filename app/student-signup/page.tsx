8d06a2afd8be10b1d6b3067cc0bcca3eb977752e
ElieMaged <Narutossj23@yahoo.com>
2024-07-09 21:08:41 +0300

    qweqwe

--- app/student-signup/page.tsx
+++ app/student-signup/page.tsx
@@ -4,15 +4,12 @@
 export default function studentSignup() {
     return(
         <>
-            <div className='flex flex-row text-black justify-center'>
-     
-
-<form className="max-w-md mx-auto">
-    
+  <div className='flex flex-row text-black justify-center'>
+<form className="max-w-md mx-auto"> 
 <div className="grid md:grid-cols-2 md:gap-6 mt-5">
     <div className="relative z-0 w-full mb-5 group">
         <input type="text"  id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-        <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 ">First name</label>
+        <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
     </div>
     <div className="relative z-0 w-full mb-5 group">
         <input type="text"  id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -30,8 +27,7 @@ export default function studentSignup() {
   
   <button type="submit" className="focus:ring-4 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center cardy text-white">Submit</button>
 </form>
-
-    </div>
+     </div>
         </>
     )
 }
