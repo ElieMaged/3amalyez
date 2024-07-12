0dbc77097fb1473f7dba4fb72fcbe34710612f7c
ElieMaged <Narutossj23@yahoo.com>
2024-07-12 20:25:01 +0300

    arab/eng

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from 'next-intl'
 
 
 export default function Home() {
-const t = useTranslations('index')
+const t = useTranslations('Index')
 
 const handleSubmit = () => {
   console.log('Translation function:', t);
@@ -15,24 +15,32 @@ console.log(t('title'))
 
   return (
  <>
-<h1></h1>
- <button onClick={handleSubmit}>HIII</button>
-    <div className="flex flex-col text-center">
-      <div className='justify-items-center'>
+  <div className="flex flex-row text-center justify-center">
+      <div className='justify-items-center '>
        <iframe className='m-auto mt-5' width="560" height="315" src="https://www.youtube.com/embed/1s_QGiK-8JM?si=5zX3j1i3y7jfWxEZ" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-       <p className='mt-4 pl-20 pr-20 mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad consequatur dignissimos quo doloribus aut iusto fuga, vel laboriosam vero minus, animi commodi error voluptate obcaecati? Maiores dolorem nulla temporibus architecto.
-       Facilis veniam harum delectus quos ipsa itaque maxime est sequi autem officiis quam commodi velit molestias excepturi, a nisi voluptates incidunt nam esse non exercitationem inventore!</p>
+       <div className='mt-4 mb-5 max-w-96 m-auto'>
+       
+       <span className='text-xl'> {t('desc')} </span>
+        <br></br>
+        <span className='text-2xl mb-2'> {t('site')}</span>
+          <br  /><span className='text-xl'>{t('desc2')}</span>
+          <br ></br><span className='text-xl'>{t('desc3')}</span>
+          <br  /> <span className='text-xl'>{t('or')}</span> 
+          <br  /> <span className='text-xl'>{t('desc4')}</span>
+          <br  /> <span className='text-2xl'>{t('desc5')}</span>
+          </div>
+       <h1 className='text-2xl mb-5'>Coming Soon</h1>
    
 
        <Link 
     href='/student-signup'
-    className='text-white font-bold py-2 px-4 border rounded mb-5 cardy'>Sign up as a Student</Link>
+    className='text-white font-bold py-2 px-4 border rounded mb-5 cardy'>{t('student')}</Link>
     <br />
-     <h2 className='mb-3 mt-3'>or</h2>
+     <h2 className='mb-3 mt-3'>{t('or')}</h2>
    
     <Link 
     href='/mentor-signup'
-    className='text-white font-bold py-2 px-4 border rounded mb-5 cardy'>Sign up as a Mentor</Link>
+    className='text-white font-bold py-2 px-4 border rounded mb-5 cardy'>{t('mentor')}</Link>
     <br></br>
     <br></br>
       </div>
